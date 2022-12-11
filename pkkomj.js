7e53a0baa494d3b4ee23e10b337ebba1a93554a4
Prizmy <112724814+Prizmy@users.noreply.github.com>
2022-12-11 16:08:23 +0300

    Update pkkomj.js

--- pkkomj.js
+++ pkkomj.js
@@ -1,4 +1,6 @@
 if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i))) {
     $(".ItemTitle span").click(function(){
     });
+    $(".galary-1").click(function(){
+    });
     }
